0e702b62fbbd61259b915d83e49cea561cc89542
valeraron <77055501+valeraron@users.noreply.github.com>
2022-01-31 20:36:38 +0200

    log

--- bin/server.js
+++ bin/server.js
@@ -1,17 +1,16 @@
 const app = require('../app')
-const mongoose = require('mongoose')
 
-const { PORT, DB_HOST } = process.env
+const { PORT } = process.env
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log('Database connection successful'))
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    }),
-  )
-  .catch(err => {
-    console.log(err.message)
-    process.exit(1)
-  })
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log('Database connection successful'))
+//   .then(() =>
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
+})
+// )
+// .catch(err => {
+//   console.log(err.message)
+//   process.exit(1)
+// })
