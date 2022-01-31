432cab2cb3401fe71b0ba047e417b9b0f9232e6c
valeraron <77055501+valeraron@users.noreply.github.com>
2022-01-31 19:58:43 +0200

    fix bag

--- bin/server.js
+++ bin/server.js
@@ -1,13 +1,17 @@
+const app = require('../app')
 const mongoose = require('mongoose')
 
-const app = require('../app')
-
-const { DB_HOST, PORT } = process.env
+const { PORT, DB_HOST } = process.env
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT), console.log('Database connection successful'))
-  .catch(error => {
-    console.log(error.message)
+  .then(() => console.log('Database connection successful'))
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    }),
+  )
+  .catch(err => {
+    console.log(err.message)
     process.exit(1)
   })
