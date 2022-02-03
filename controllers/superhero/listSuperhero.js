83343a65a5976536ec3f3a9db8ee1822e3450a00
valeraron <77055501+valeraron@users.noreply.github.com>
2022-02-03 18:11:30 +0200

    return change

--- controllers/superhero/listSuperhero.js
+++ controllers/superhero/listSuperhero.js
@@ -3,12 +3,16 @@ const { Superhero } = require('../../models')
 const listSuperhero = async (req, res) => {
   const { page = 1, limit = 5 } = req.query
   const skip = (page - 1) * limit
-  const superhero = await Superhero.find({}, '', {
+  const result = await Superhero.find({}, '', {
     skip,
     limit: Number(limit),
   })
   res.json({
-    superhero,
+    status: 'success',
+    code: 200,
+    data: {
+      result,
+    },
   })
 }
 
