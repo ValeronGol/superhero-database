89483276c8015fff7e1bbe2502412306e7543456
valeraron <77055501+valeraron@users.noreply.github.com>
2022-02-27 18:28:56 +0200

    fixed

--- models/superhero.js
+++ models/superhero.js
@@ -29,6 +29,10 @@ const Superhero = model('superhero', superheroSchema)
 
 const joiSchema = Joi.object({
   nickname: Joi.string().required(),
+  real_name: Joi.string().required(),
+  origin_description: Joi.string().required(),
+  superpowers: Joi.string().required(),
+  catch_phrase: Joi.string().required(),
   images: Joi.string().required(),
 })
 
